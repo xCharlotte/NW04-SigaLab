65060c5249b8aaa2a04ec9491de5fe23cad0f030
Charlotte <Charlottevoskuilen@hotmail.com>
2017-11-15 13:22:11 +0100

    Added routes for admin prefix and added a view

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -13,8 +13,18 @@ class Admin
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $guard = null)
     {
+      if (Auth::guard($guard)->guest()) {
+        if ($request->ajax()) {
+          return response('Unauthorized.', 401);
+        } else {
+          return redirect()->guest('login');
+        }
+      } else if (!Auth::guard($guard)->user()->is_admin) {
+        return redirect()->to('/')->withError('Permission Denied');
+      }
+
         return $next($request);
     }
 }
