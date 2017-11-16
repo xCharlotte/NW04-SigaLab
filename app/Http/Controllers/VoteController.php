e16d1af93ac609531bc589837efad31fcf2f2bd3
Raymonddv <raymond_max@hotmail.com>
2017-11-16 11:57:33 +0100

    view image test

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -13,7 +13,7 @@ class VoteController extends Controller
      */
     public function index()
     {
-        //
+        return view('vote');
     }
 
     /**
--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 
-
+<div class="container">
+<h1>HAllo</h1>
+<img src="{{ asset('images/communication.png') }}" class="img-fluid" alt="Boeke">
+</div>
 
 @endsection
