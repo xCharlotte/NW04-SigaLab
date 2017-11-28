9923c5d80fa2188cf28ea6f5b144244fd45712fd
Charlotte <Charlottevoskuilen@hotmail.com>
2017-11-28 13:57:22 +0100

    Changed style of admin and user view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard ADMIN</div>
 
-  <h1>Dashboard admin</h1>
-
+                <div class="panel-body">
+                  Ingelogd als ADMIN
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
