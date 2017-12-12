58d106c4e07c5ca9d17d31a6a0d5db10edca4a07
Charlotte <Charlottevoskuilen@hotmail.com>
2017-12-12 13:06:42 +0100

    Added admin api cause we are going to use api anyway

--- config/auth.php
+++ config/auth.php
@@ -50,6 +50,11 @@ return [
             'driver' => 'session',
             'provider' => 'admins',
         ],
+
+        'admin-api' => [
+          'driver' => 'token',
+          'provider' => 'admins',
+        ],
     ],
 
     /*
